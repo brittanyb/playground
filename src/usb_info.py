06390bcbd0ac28cb05a7d0467319dd1f0b70d7a4
Brittany Barrett <brittany.l.barrett@gmail.com>
2023-09-25 17:36:17 +0100

    Add a basic description for RE:Flex Dance Pad data

---
+++ src/usb_info.py
@@ -0,0 +1,19 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class HIDInfo:
+    """Base data class for a Custom HID implementation."""
+
+    VID: int
+    PID: int
+    LIGHTS_EP: int
+    SENSORS_EP: int
+    BYTES: int
+
+
+class ReflexV2Info(HIDInfo):
+    """HID information for a RE:Flex v2 Dance Pad."""
+
+    def __init__(self):
+        super(ReflexV2Info, self).__init__(0x0483, 0x5750, 0x01, 0x81, 64)
