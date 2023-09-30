76d18816e9612e0d470595a5eccee86aa609a49d
Brittany Barrett <brittany.l.barrett@gmail.com>
2023-09-30 18:34:27 +0100

    Add method calls to connection widget

---
+++ src/connection_widget.py
@@ -0,0 +1,80 @@
+import PySide6.QtWidgets as QtWidgets
+
+from reflex_controller import ReflexController
+
+
+class ConnectionWidget(QtWidgets.QWidget):
+    """Widget to manage an open connection to a dance pad."""
+
+    def __init__(self):
+        super(ConnectionWidget, self).__init__()
+        self._controller = ReflexController()
+        self._active = False
+        self.refresh_icon = QtWidgets.QStyle.StandardPixmap.SP_BrowserReload
+        self.connect_icon = QtWidgets.QStyle.StandardPixmap.SP_MediaPlay
+        self.disconnect_icon = QtWidgets.QStyle.StandardPixmap.SP_MediaStop
+        self._refresh = QtWidgets.QToolButton()
+        self._refresh.setIcon(self.style().standardIcon(self.refresh_icon))
+        self._refresh.clicked.connect(self.refresh_pads)
+        self._connect = QtWidgets.QToolButton()
+        self._connect.setIcon(self.style().standardIcon(self.connect_icon))
+        self._connect.clicked.connect(self.toggle_pad_connection)
+        self._dropdown = QtWidgets.QComboBox()
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self._refresh)
+        layout.addWidget(self._connect)
+        layout.addWidget(self._dropdown)
+        layout.setContentsMargins(1, 1, 1, 1)
+        self.setLayout(layout)
+        self.refresh_pads()
+
+    def _set_connect_button(self) -> None:
+        if self._active:
+            icon = QtWidgets.QStyle.StandardPixmap.SP_MediaStop
+        else:
+            icon = QtWidgets.QStyle.StandardPixmap.SP_MediaPlay
+        self._connect.setIcon(self.style().standardIcon(icon))
+        self._connect.update()
+        if not len(self._controller.all_pads):
+            self._connect.setDisabled(True)
+            return
+        self._connect.setEnabled(True)
+
+    def _set_dropdown(self) -> None:
+        if self._active:
+            self._dropdown.setDisabled(True)
+            return
+        for _ in range(self._dropdown.count()):
+            self._dropdown.removeItem(0)
+        serials = [s for s in self._controller.all_pads if isinstance(s, str)]
+        if len(serials):
+            for serial in sorted(serials):
+                if serial:
+                    self._dropdown.addItem(serial)
+        self._dropdown.setEnabled(True)
+
+    def _set_refresh(self) -> None:
+        if self._active:
+            self._refresh.setDisabled(True)
+            return
+        self._refresh.setEnabled(True)
+
+    def _set_widgets(self) -> None:
+        self._set_connect_button()
+        self._set_dropdown()
+        self._set_refresh()
+
+    def refresh_pads(self) -> None:
+        self._controller.enumerate_pads()
+        self._set_widgets()
+
+    def toggle_pad_connection(self) -> None:
+        if self._active:
+            if pad := self._controller.connected_pads[0]:
+                self._controller.disconnect_pad(pad.serial)
+                self._active = False
+        else:
+            serial = self._dropdown.itemText(self._dropdown.currentIndex())
+            if self._controller.connect_pad(serial):
+                self._active = True
+        self._set_widgets()
--- src/pad_gui.py
+++ src/pad_gui.py
@@ -3,44 +3,17 @@ import PySide6.QtWidgets as QtWidgets
 import qdarktheme
 import sys
 
+from connection_widget import ConnectionWidget
 from pad_widget_controller import PadWidgetController
 from pad_widget_view import PadWidgetView
 from pad_model import PadModel
 from test_data_generator import TestPadWidget
 
 
-class PadConnection(QtWidgets.QWidget):
-    def __init__(self):
-        super(PadConnection, self).__init__()
-        pad_refresh = QtWidgets.QToolButton()
-        refresh_icon = QtWidgets.QStyle.StandardPixmap.SP_BrowserReload
-        pad_refresh.setIcon(self.style().standardIcon(refresh_icon))
-        pad_connection = QtWidgets.QToolButton()
-        play_icon = QtWidgets.QStyle.StandardPixmap.SP_MediaPlay
-        pad_connection.setIcon(self.style().standardIcon(play_icon))
-        pad_dropdown = QtWidgets.QComboBox()
-        pad_dropdown.setPlaceholderText("Brittany's Dance Pad")
-        layout = QtWidgets.QHBoxLayout()
-        layout.addWidget(pad_refresh)
-        layout.addWidget(pad_connection)
-        layout.addWidget(pad_dropdown)
-        layout.setContentsMargins(1, 1, 1, 1)
-        self.setLayout(layout)
-
-    def refresh_pads(self):
-        pass
-
-    def toggle_pad_connection(self):
-        pass
-
-    def update_pad_list(self):
-        pass
-
-
 class MainWidget(QtWidgets.QWidget):
     def __init__(self):
         super(MainWidget, self).__init__()
-        pad_connection = PadConnection()
+        pad_connection = ConnectionWidget()
         self.pad_model = PadModel()
         self.pad_view = PadWidgetView()
         self.pad_controller = PadWidgetController(
@@ -50,7 +23,6 @@ class MainWidget(QtWidgets.QWidget):
         layout.addWidget(pad_connection)
         layout.addWidget(self.pad_controller)
         self.setLayout(layout)
-        self.tester = TestPadWidget(self.pad_model, self.pad_controller)
 
 
 class MainWindow(QtWidgets.QMainWindow):
