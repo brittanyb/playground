169fefbf0123b593461e369f4159b431c67f3f12
Brittany Barrett <brittany.l.barrett@gmail.com>
2023-09-30 18:32:40 +0100

    Separate USB from pad, create endpoint processes

--- src/usb_controller.py
+++ src/usb_controller.py
@@ -1,11 +1,10 @@
 import multiprocessing
-import time
 
 import libusb_package
 import usb.core
 import usb.backend.libusb1
 
-from usb_info import HIDInfo, ReflexV2Info
+from usb_info import HIDInfo
 
 
 class USBDeviceList:
@@ -34,37 +33,55 @@ class USBDeviceList:
                 return device
 
 
-class DancePadProcess(multiprocessing.Process):
-    """Separate process runner for a dance pad with custom I/O IPC."""
+class HIDEndpointProcess(multiprocessing.Process):
+    """Base class that manages a single HID endpoint in its own process."""
 
     def __init__(
-            self, pad_info: HIDInfo, serial: str,
-    ):
-        super(DancePadProcess, self).__init__()
+            self, pad_info: HIDInfo, serial: str):
+        super(HIDEndpointProcess, self).__init__()
         self._info = pad_info
         self._serial = serial
+        self._queue = multiprocessing.Queue(maxsize=10)
+        self._device = None
+        self.start()
 
     def run(self) -> None:
-        index = 0
         self._device = USBDeviceList.get_pad_by_serial(
             self._info.VID, self._info.PID, self._serial
         )
-        while (True):
+        while True:
             if not self._device:
-                break
-            sensor_data = self._device.read(
-                self._info.SENSORS_EP, self._info.BYTES
-            )
-            index += 1
-            if (index % 500 == 0):
-                print(sensor_data)
+                return
+            try:
+                self._process()
+            except Exception as e:
+                raise e
+
+    def _process(self) -> str | None:
+        pass
+
+    @property
+    def queue(self) -> multiprocessing.Queue:
+        return self._queue
 
 
-if __name__ == "__main__":
-    pad_info = ReflexV2Info()
-    serials = USBDeviceList.connected_pad_names(pad_info)
-    if isinstance(serials[0], str):
-        process = DancePadProcess(pad_info, serials[0])
-        process.start()
-        time.sleep(5)
-        process.terminate()
+class HIDReadProcess(HIDEndpointProcess):
+    """Child class for reading sensor data from an HID Endpoint."""
+
+    def _process(self) -> str | None:
+        self._device: usb.core.Device
+        sensor_data = self._device.read(
+            self._info.SENSORS_EP, self._info.BYTES
+        )
+        if self._queue.full():
+            self._queue.get_nowait()
+        self._queue.put_nowait(sensor_data)
+
+
+class HIDWriteProcess(HIDEndpointProcess):
+    """Child class for writing light data to an HID Endpoint."""
+
+    def _process(self) -> str | None:
+        self._device: usb.core.Device
+        light_data = self._queue.get_nowait()
+        self._device.write(self._info.LIGHTS_EP, light_data)
