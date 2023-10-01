e15b99f9b230e39bda053aa86a7a41f4c1feea79
Brittany Barrett <brittany.l.barrett@gmail.com>
2023-10-01 10:51:59 +0100

    Rename pad_gui to application, remove view refs

--- src/application.py
+++ src/application.py
@@ -1,48 +1,48 @@
+import sys
+
 import PySide6.QtGui as QtGui
 import PySide6.QtWidgets as QtWidgets
 import qdarktheme
-import sys
 
 from connection_widget import ConnectionWidget
-from pad_widget_controller import PadWidgetController
-from pad_widget_view import PadWidgetView
+from profile_widget import ProfileWidget
+from profile_controller import ProfileController
+from pad_widget import PadWidget
 from pad_model import PadModel
-from test_data_generator import TestPadWidget
 
 
 class MainWidget(QtWidgets.QWidget):
+    """Central widget containing all Pad GUI widgets."""
+
     def __init__(self):
         super(MainWidget, self).__init__()
         pad_connection = ConnectionWidget()
         self.pad_model = PadModel()
-        self.pad_view = PadWidgetView()
-        self.pad_controller = PadWidgetController(
-            self.pad_model, self.pad_view
-        )
+        self.pad_widget = PadWidget(self.pad_model)
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(pad_connection)
-        layout.addWidget(self.pad_controller)
+        layout.addWidget(self.pad_widget)
         self.setLayout(layout)
 
 
 class MainWindow(QtWidgets.QMainWindow):
+    """Main window for Pad GUI."""
+
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.setFixedSize(858, 895)
+        widget = MainWidget()
         self.setWindowTitle("RE:Flex Dance - Playground")
-        self.setCentralWidget(MainWidget())
+        self.setCentralWidget(widget)
+        self.setFixedSize(widget.width(), widget.height())
 
 
 class MainApplication(QtWidgets.QApplication):
+    """Application entry point for Pad GUI."""
+
     def __init__(self):
         super(MainApplication, self).__init__(sys.argv)
         self.set_opengl_doublebuffering()
-        self.setWindowIcon(QtGui.QIcon("../assets/favicon.ico"))
-        qdarktheme.setup_theme(custom_colors={"primary": "#ad02ff"})
-        palette = self.palette()
-        window_text = QtGui.QPalette.ColorRole.WindowText
-        palette.setColor(window_text, QtGui.QColor(255, 255, 255))
-        self.setPalette(palette)
+        self.set_application_theme()
         self.window = MainWindow()
         self.window.show()
 
@@ -52,6 +52,14 @@ class MainApplication(QtWidgets.QApplication):
         format.setSwapInterval(1)
         format.setSwapBehavior(QtGui.QSurfaceFormat.SwapBehavior.DoubleBuffer)
         QtGui.QSurfaceFormat.setDefaultFormat(format)
+
+    def set_application_theme(self):
+        self.setWindowIcon(QtGui.QIcon("../assets/favicon.ico"))
+        qdarktheme.setup_theme(custom_colors={"primary": "#ad02ff"})
+        palette = self.palette()
+        window_text = QtGui.QPalette.ColorRole.WindowText
+        palette.setColor(window_text, QtGui.QColor(255, 255, 255))
+        self.setPalette(palette)
 
 
 if __name__ == "__main__":
