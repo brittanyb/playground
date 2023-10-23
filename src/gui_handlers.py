57f1bffc259891d66a4d4ced115a8994b011ca5b
Brittany Barrett <brittany.l.barrett@gmail.com>
2023-10-23 19:56:17 +0100

    Create base widget handlers

---
+++ src/gui_handlers.py
@@ -0,0 +1,36 @@
+from connection_widget import ConnectionWidget
+from pad_widget import PadWidget
+from profile_widget import ProfileWidget
+
+
+class GUIHandlers:
+    """Methods to run on widgets on data process requests."""
+
+    def __init__(
+        self, connection_widget: ConnectionWidget, pad_widget: PadWidget,
+        profile_widget: ProfileWidget
+    ):
+        self._connection_widget = connection_widget
+        self._pad_widget = pad_widget
+        self._profile_widget = profile_widget
+        self.initialise_widgets()
+
+    def initialise_widgets(self) -> None:
+        self._connection_widget.set_connect_button_state(False)
+
+    def all_pads_received(self, all_pads: list[str]) -> None:
+        pads_available = len(all_pads) > 0
+        self._connection_widget.set_dropdown_state(pads_available)
+        self._connection_widget.set_dropdown_items(all_pads)
+        self._connection_widget.set_connect_button_state(pads_available)
+
+    def profile_names_received(self, profile_names: list[str]) -> None:
+        self._profile_widget.set_dropdown_items(profile_names)
+
+    def pad_connected(self, connected: bool) -> None:
+        if connected:
+            active = False
+        else:
+            active = True
+        self._connection_widget.set_dropdown_state(active)
+        self._connection_widget.set_connect_button_icon(active)
