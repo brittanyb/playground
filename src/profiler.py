de700df5c033f6613aeb360234be7020704a6540
Brittany Barrett <brittany.l.barrett@gmail.com>
2023-11-01 19:00:36 +0000

    Add async profiler

---
+++ src/profiler.py
@@ -0,0 +1,25 @@
+import asyncio
+import cProfile
+import sys
+import threading
+
+
+class Profiler:
+    def __init__(self, timeout: int, fn: str):
+        self._fn = fn
+        self._timeout = timeout
+        self.pr = cProfile.Profile()
+        self.pr.enable()
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        threading.Thread(target=self.run_asyncio_loop, daemon=True).start()
+
+    def run_asyncio_loop(self):
+        self.loop.run_until_complete(asyncio.sleep(self._timeout))
+        self.end_profile()
+
+    def end_profile(self):
+        self.pr.disable()
+        with open(self._fn, 'w') as f:
+            sys.stdout = sys.stderr = f
+            self.pr.print_stats('cumulative')
