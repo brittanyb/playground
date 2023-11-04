573f8bc152df18e2af74b62a754e6e96c0e86311
Brittany Barrett <brittany.l.barrett@gmail.com>
2023-11-04 14:29:03 +0000

    Create rudimentary USB profiling

--- src/led_data_handler.py
+++ src/led_data_handler.py
@@ -1,4 +1,5 @@
 import multiprocessing
+import time
 
 
 class LEDDataHandler:
@@ -6,6 +7,25 @@ class LEDDataHandler:
 
     def __init__(self, light_queue: multiprocessing.Queue):
         self._light_queue = light_queue
+        self._samples = 0
+        self._last_time = time.time()
+        self._delta = 0
+        self._first_time = False
 
     def give_sample(self):
-        pass
+        if self._light_queue.empty():
+            self._light_queue.put(bytearray(64))
+            return self.count_samples()
+        return None
+
+    def count_samples(self):
+        self._samples += 1
+        if self._samples % 1000 == 0:
+            if self._first_time == False:
+                self._last_time = time.time()
+                self._first_time = True
+                return None
+            current_time = time.time()
+            self._delta += current_time - self._last_time - 1
+            self._last_time = current_time
+            return self._delta
--- src/sensor_data_handler.py
+++ src/sensor_data_handler.py
@@ -9,12 +9,23 @@ class SensorDataHandler:
         self._sensor_queue = sensor_queue
         self._samples = 0
         self._last_time = time.time()
+        self._delta = 0
+        self._first_time = False
 
     def take_sample(self):
         if not self._sensor_queue.empty():
             self._sensor_queue.get()
-            self._samples += 1
-            if self._samples % 1000 == 0:
-                current_time = time.time()
-                print(f"1k samples in: {current_time - self._last_time}s")
-                self._last_time = current_time
+            return self.count_samples()
+        return None
+
+    def count_samples(self):
+        self._samples += 1
+        if self._samples % 1000 == 0:
+            if self._first_time == False:
+                self._last_time = time.time()
+                self._first_time = True
+                return None
+            current_time = time.time()
+            self._delta += current_time - self._last_time - 1
+            self._last_time = current_time
+            return self._delta
